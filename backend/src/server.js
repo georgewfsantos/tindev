ade8dbac160dc7aa3bb0ce5a79120c3e1bc96881
George W F Santos <gweverton21@hotmail.com>
2020-02-18 18:20:12 -0300

    add web-socket functionality

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+
 const cors = require("cors");
-const server = express();
+
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user } = socket.handshake.query;
+  connectedUsers[user] = socket.id;
+});
 
 mongoose.connect(
   "mongodb+srv://tindevomnistack:tindevomnistack@cluster0-zfcwj.mongodb.net/tindevomnistack?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
